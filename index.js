644b8d75b35e2f2641c704f513b4435ba414ae07
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-05-26 20:26:49 -0400

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function newImage(url, left, bottom){
+function newImage(url, left, bottom) {
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
@@ -8,11 +8,11 @@ function newImage(url, left, bottom){
     return object
 }
 
-function newItem(url, left, bottom){
+function newItem(url, left, bottom) {
     let item = newImage(url, left, bottom)
 }
 
-function newInventory(){
+function newInventory() {
     let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
